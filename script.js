58df78f7ebd2c0f3e412d9d16c6bb984e012fd6b
RodUman1985 <108888005+RodUman1985@users.noreply.github.com>
2022-10-31 21:11:33 +0300

    3

--- script.js
+++ script.js
@@ -44,3 +44,81 @@ let arr3=[1,2,true,"hello",5];
 console.log(arr3[3]); 
 console.log(arr3[2]); 
 console.log(arr3[0]); 
+//console.clear;
+//циклы
+//while
+let i=0;
+while(i<4){ 
+    console.log(i);
+    i++; 
+}
+//do/while
+do{
+    console.log('do-while'); 
+}while(i>100);
+// for
+for(let i=0;i<5;i++){
+    console.log(i*i)
+}
+//очистка консоли
+//console.clear;
+//for-in
+for(let e in arr2){// в переменную e  будут записываться индексы массива
+   //console.log(e);
+    console.log(arr2[e]);
+}
+//for -of
+for(let e of arr3){// в переменную e  будут записываться значения элементов массива
+   
+     console.log(e);
+ }
+ // if-else
+ //if(a>b){
+ //   ...a.}
+ //else{
+//  ...a.}
+ //switch-case
+ /*switch(a){
+    case1:/.../
+    case...n:/../
+    break; ()//обязательно
+    default:/.../
+ }*/
+ //функции*/
+ console.clear;
+ function sum(x,y){
+   return x+y;
+ }
+ console.log(sum(2,5));
+ function max(a,b,c){
+    if(a==undefined){
+        return undefined;
+    }
+    if(b==undefined){
+        return a;
+    }
+    if(c==undefined){
+        if(a>b){
+            return a
+        }
+        return b;
+    }
+    if(a>b && a>c){
+        return a;
+    }
+    if(b>c){
+        return b;
+    }
+    return c;
+ }
+console.log(max());
+console.log(max(1));
+console.log(max(1,2));
+console.log(max(1,2,3));
+//p=0..+
+function total(bill,p = 15){
+    return bill*(1+p/100);
+}
+console.log(total(200,10))
+console.log(total(200))
+//если аргументу заданно значение по умолчанию то всем последующим тоже должно быть заданно значение по умолчанию
