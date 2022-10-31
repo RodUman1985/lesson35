e1e515f58500ac452b8eb3e86993b6adeda98238
RodUman1985 <108888005+RodUman1985@users.noreply.github.com>
2022-10-31 19:57:31 +0300

    2

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ const r = 12;
 //r=20; // ошибка
 console.log(r);
 b=15;
+// типы данных поддерживаемые в javaScript
+//string - любая последовательность символов заключенная в двойные, одинарные либо косые ковычки
+let t1="For";
+let t2='the';
+let t3=`Emperor!!!!`;
+// typeof - возвращает название типа
+console.log(typeof t3);
+// number -  хранит любое целое либо вещественное число
+let n1= 12;
+let n2= 3.14;
+console.log(typeof n1);
+console.log(typeof n2);
+// boolean - ture/false булеевская переменная, истина/лож
+let b1=true;
+let b2=false;
+console.log(typeof b1);
+console.log(typeof b2);
+// основные арифметические опператоры
+//[ все как в Java]
+console.log(1+7*8-3);
+// массивы 
+let arr=[];// объявление пустого массива
+let arr2=[1,2,3,4,5]; // объявление  массива с элементами
+console.log(arr2[3]); 
+console.log(arr2.length);
+let arr3=[1,2,true,"hello",5]; 
+console.log(arr3[3]); 
+console.log(arr3[2]); 
+console.log(arr3[0]); 
